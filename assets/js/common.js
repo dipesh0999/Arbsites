f7b1831f1af11f29b42e56b39fb65684cb807590
Office desktop <javed.mistari@webmaffia.com>
2023-06-05 12:18:39 +0530

    mobile menu in desktop

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,6 +3,34 @@
 
     // write code here
 
+    $(".mobMenu .menu").click(function(e){
+      $(this).text("Close");
+      $(this).addClass("active");
+    });
+
+    $(".mobMenu .active").click(function(e){
+      $(this).text("Menu");
+      $(this).removeClass("active");
+    });
+
+    $(".slickSlider").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+      if (currentSlide === 0) {
+        $('.testimonial .slick-prev').css('opacity', '0.3');
+      }
+      if (currentSlide === 1) {
+        $('.testimonial .slick-prev').css('opacity', '1');
+      }
+    });
+
+    $(".slickSlider").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+      if (currentSlide === 2) {
+        $('.testimonial .slick-next').css('opacity', '1');
+      }
+      if (currentSlide === 3) {
+        $('.testimonial .slick-next').css('opacity', '0.3');
+      }
+    });
+
     $('.testimonial .slickSlider').slick({
       dots: false,
       infinite: false,
@@ -78,54 +106,19 @@
       ]
     });
 
-    // $('.testimonial .slickSlider').slick({
-    //   slidesToShow: 3,
-    //   slidesToScroll: 1,
-    //   autoplay: false,
-    //   infinite: false,
-    //   autoplaySpeed: 20000,
-    //   speed: 1000,
-    //   prevArrow: $('.slick-prev'),
-    //   nextArrow: $('.slick-next'),
-    //   resposive:[
-    //     {
-    //       breakpoint: 1024,
-    //       settings: {
-    //         slidesToShow: 3,
-    //         slidesToScroll: 1,
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 600,
-    //       settings: {
-    //         slidesToShow: 2,
-    //         slidesToScroll: 2
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 480,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         centerMode: false
-    //       }
-    //     }
-    //   ]
-    // });
-
     const $body = $('body');
 
     let oldScroll = 0;
 
     $('html').removeClass('no-js');
 
-    $body.on('click', '.menu', function (e) {
-        $body.toggleClass('.menuActive');
-    });
+    // $body.on('click', '.menu', function (e) {
+    //     $body.toggleClass('.menuActive');
+    // });
 
-    $body.on('click', '.menu', function (e) {
-        $body.removeClass('menu-open');
-    });
+    // $body.on('click', '.menu', function (e) {
+    //     $body.removeClass('menu-open');
+    // });
 
     document.addEventListener('scroll', e => {
         var wTop = document.documentElement.scrollTop || document.body.scrollTop;
